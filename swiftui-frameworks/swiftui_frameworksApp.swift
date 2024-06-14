87bb6c616ece439c4fd0a37e3511a90098bd8993
Chris Salvador <csalvador58@gmail.com>
2024-06-14 16:11:12 -0700

    Initial Commit

---
+++ swiftui-frameworks/swiftui_frameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_frameworksApp.swift
+//  swiftui-frameworks
+//
+//  Created by Salvador on 6/14/24.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_frameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
