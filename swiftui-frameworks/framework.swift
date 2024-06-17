91756101024f103299011736bbb032d7ce1d64da
Chris Salvador <csalvador58@gmail.com>
2024-06-16 23:50:43 -0700

    Initial commit

---
+++ swiftui-frameworks/framework.swift
@@ -0,0 +1,15 @@
+//
+//  framework.swift
+//  swiftui-frameworks
+//
+//  Created by Salvador on 6/14/24.
+//
+
+import Foundation
+
+struct Framework {
+    let name: String
+    let imageName: String
+    let urlString: String
+    let description: String
+}
