82efbe9a2cc5f5584d8334689b21fb1f15e7f7ab
Chris Salvador <csalvador58@gmail.com>
2024-06-17 02:03:15 -0700

    added scroll view

--- swiftui-frameworks/FrameworkGridView.swift
+++ swiftui-frameworks/FrameworkGridView.swift
@@ -8,8 +8,25 @@
 import SwiftUI
 
 struct FrameworkGridView: View {
+    
+    let columns: [GridItem] = [GridItem(.flexible()),
+                               GridItem(.flexible()),
+                               GridItem(.flexible())]
+    
     var body: some View {
-        FrameworkTitleView(name: "App Clips", imageName: "app-clip")
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    // Since MockData is identifiable, able to remove id: \.id
+                    ForEach(MockData.frameworks) { framework in
+                        FrameworkTitleView(framework: framework)
+                    }
+                }
+                
+            }
+            .navigationTitle("🍎 Frameworks")
+        }
+        
     }
 }
 
@@ -19,20 +36,20 @@ struct FrameworkGridView: View {
 
 struct FrameworkTitleView: View {
     
-    let name: String
-    let imageName: String
+    let framework: Framework
     
     var body: some View {
         VStack {
-            Image(imageName)
+            Image(framework.imageName)
                 .resizable()
                 .frame(width: 90, height: 90)
-            Text(name)
+            Text(framework.name)
                 .font(.title2)
                 .fontWeight(.semibold)
                 .scaledToFit()
                 .minimumScaleFactor(0.6)
         }
+        .padding()
     }
     
     
--- swiftui-frameworks/framework.swift
+++ swiftui-frameworks/framework.swift
@@ -7,7 +7,9 @@
 
 import Foundation
 
-struct Framework {
+struct Framework: Hashable, Identifiable {
+//    unique identifier for Framework object
+    let id = UUID()
     let name: String
     let imageName: String
     let urlString: String
